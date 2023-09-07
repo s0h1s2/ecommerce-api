4efbd33afd460795845940f8317757b139c02168
shkar <shkarhsardar@gmail.com>
2023-09-07 11:24:00 +0300

    removed dist directory

--- dist/route/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_1 = require("../controller/product");
-const router = (0, express_1.Router)();
-router.get("/", product_1.getProducts);
-router.get("/:id(\\d+)/", product_1.getProductById);
-exports.default = router;
