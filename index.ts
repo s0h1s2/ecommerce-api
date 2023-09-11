f030519dd35f34c2255ea6f6a4795c0b612ac365
shkar <shkarhsardar@gmail.com>
2023-09-11 20:20:52 +0300

    remove slash

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const port = 3000
 app.use(express.json())
 app.use(cookieParser())
 // TODO: change origin when deploy
-app.use(cors({ origin: "https://master--subtle-jelly-5c6ee7.netlify.app/", credentials: true }))
+app.use(cors({ origin: "https://master--subtle-jelly-5c6ee7.netlify.app", credentials: true }))
 app.use("/api", router)
 app.listen(port, () => {
 	console.log(`[Server] Server listening on port ${port}`)
