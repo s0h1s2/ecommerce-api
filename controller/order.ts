c1e2363b15ead4459df6f61dbd2a361b7af0715a
shkar <shkarhsardar@gmail.com>
2023-09-11 21:39:47 +0300

    added general error handler

--- controller/order.ts
+++ controller/order.ts
@@ -16,38 +16,40 @@ export function getOrderById(req: Request, res: Response) {
 export async function createOrder(req: Request, res: Response) {
   const { orderItems, address, city, country, postalCode }: CreateOrder = req.body
   try {
-    const products = await Promise.all(orderItems.map(async (product, index) => {
-      const result = await db.product.findFirstOrThrow({ where: { id: product.productId } })
-      if (result.stock - product.qty < 0) {
-        throw new Error("Product stock unavaliable.")
-      }
-      return { id: result.id, qty: product.qty, price: result.price }
-    }))
-    const totalPrice = products.reduce((p, c) => c.price * c.qty + p, 0)
-    // TODO: may write function to calcuate total price from shippingPrice and itemsPrice and taxPrice.
-    // TODO: maybe using transaction a better idea
-    const orderResult = await db.order.create({
-      data: {
-        totalPrice: totalPrice*1,
-        address,
-        city,
-        country,
-        postalCode,
-        itemsPrice: totalPrice,
-        orderItems: {
-          create: products.map((p) => ({ qty: p.qty }))
-        },
-        shippingPrice: totalPrice,
-        taxPrice: 1,
-        isPaid: false,
-        isDelivered: false,
-        userId: req.user.id,
-        deliveredTime: new Date()
-      }
-    })
-    // TODO: may update stock then send web socket request to client to update items stock
-    products.map(async (p) => {
-      await db.product.update({ where: { id: p.id }, data: { stock: { decrement: p.qty } } })
+    const orderResult = await db.$transaction(async (db) => {
+      const products = await Promise.all(orderItems.map(async (product, index) => {
+        const result = await db.product.findFirstOrThrow({ where: { id: product.productId } })
+        if (result.stock - product.qty < 0) {
+          throw new Error("Product stock unavaliable.")
+        }
+        return { id: result.id, qty: product.qty, price: result.price }
+      }))
+      const totalPrice = products.reduce((p, c) => c.price * c.qty + p, 0)
+
+      products.map(async (p) => {
+        await db.product.update({ where: { id: p.id }, data: { stock: { decrement: p.qty } } })
+      })
+      const orderResult = await db.order.create({
+        data: {
+          totalPrice: totalPrice * 1,
+          address,
+          city,
+          country,
+          postalCode,
+          itemsPrice: totalPrice,
+          orderItems: {
+            create: products.map((p) => ({ qty: p.qty }))
+          },
+          shippingPrice: totalPrice,
+          taxPrice: 1,
+          isPaid: false,
+          isDelivered: false,
+          userId: req.user.id,
+          deliveredTime: new Date()
+        }
+      })
+      // TODO: may update stock then send web socket request to client to update items stock
+      return orderResult
     })
     return res.status(StatusCodes.CREATED).send({ order: orderResult })
   } catch (e: any) {
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import express, { Express } from "express"
+import express, { Express, NextFunction, Request,Response } from "express"
 import router from "./route"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import { UserRequest } from "./interfaces/UserRequest"
+import methodOverride from "method-override"
 declare global {
 	namespace Express {
 		interface Request {
@@ -16,7 +17,15 @@ app.use(express.json())
 app.use(cookieParser())
 // TODO: change origin when deploy
 app.use(cors({ origin: "https://master--subtle-jelly-5c6ee7.netlify.app", credentials: true }))
+app.use(methodOverride())
+function errorHandler (err:any, req:Request, res:Response, next:NextFunction) {
+	res.status(500).send({error:"Something is broken."})
+	next()
+}
+  
+app.use(errorHandler)
 app.use("/api", router)
+
 app.listen(port, () => {
 	console.log(`[Server] Server listening on port ${port}`)
 })
